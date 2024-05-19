0a835910cd743a349defc28d1e2e519e75d3887b
kroszczak <k_roszczak@icloud.com>
2024-05-19 16:16:34 +0200

    added parameters

--- RK4.py
+++ RK4.py
@@ -1,63 +1,65 @@
 import matplotlib.pyplot as plt
+from RPNparser import infix_to_rpn
 import numpy as np
 import copy
+from sympy import sympify, symbols
+import yaml
+from datetime import datetime, date
+import math
 
-# tablica zmiennych - dane początkowe
-s = [ 0,0,0,0 ]
-labels = ['p53', 'MDMcyt', 'MDMn', 'PTEN']
-
+# Wczytaj plik YAML
+sub_dict = dict()
+fin_dict = dict()
+funtions = dict()
+data = None
+with open("model.yaml", 'r') as file:
+    data = yaml.safe_load(file)
+for key, value in data['parameters'].items():
+    sub_dict[symbols(key)] = value
+for key, value in data['factors'].items():
+    fin_dict[key] = value['starting_value']
+for key, value in data['factors'].items():
+    args = ",".join([i for i in list(sorted(data['factors'].keys()))])
+    funtions[key] = eval(f"lambda {args}: {sympify(value['function']).subs(sub_dict)}")
+# RK SIMULATION
 def pochodne(s,k):
-    # Stałe
-    p1 = 8.8
-    p2 = 440
-    # p2 = 440 * 0.02 # włączone siRNA
-    p3 = 100
-    p3 = 0 # bez PTEN
-    d1 = 1.375 * 10**-14
-    d2 = 1.375 * pow(10,-4) * 0.1
-    d2 = 1.375 * pow(10,-4) # uszkodzenie DNA
-    d3 = 3 * pow(10,-5)
-    k1 = 1.925 * pow(10,-4)
-    k2 = pow(10,5)
-    k3 = 1.5 * pow(10,5)
+    for key, val in funtions.items(): k[key] = val(**fin_dict)
 
-    # wzory na kolejne zmienne
-    k[0] = p1 - (d1 * s[0] * pow(s[2],2))
-    k[1] = p2 * ( pow(s[0],4) / (pow(s[0],4) + pow(k2,4)) ) - ( k1 * ( pow(k3,2) / (pow(k3,2) + pow(s[3],2)) ) * s[1] ) - (d2 * s[1])
-    k[2] = (k1 * pow(k3,2) * s[1]) / (pow(k3,2) + pow(s[3],2)) - (d2 * s[2])
-    k[3] = p3 * (pow(s[0],4) / (pow(s[0],4) + pow(k2,4))) - (d3 * s[3])
-
-
-def rk4_vec(t, dt, n, s, f):
-    rk_step_matrix = [np.zeros(len(s)) for i in range(5)]
-    for j in range(4):
-        for i, factor in zip(range(n), [0,dt/2*rk_step_matrix[0][j-1],dt/2*rk_step_matrix[1][j-1],dt*rk_step_matrix[2][j-1]]):
-            rk_step_matrix[4][i]=s[i]+factor
+def rk4_vec(hd):
+    for j, factor in loop:
+        prev = rk_step_matrix[j-1]
+        for i in funtions.keys():
+            ifactor = factor*prev.get(i,0)
+            rk_step_matrix[4][i]=fin_dict[i]+ifactor
         pochodne(rk_step_matrix[4], rk_step_matrix[j])
-    for i in range(n): s[i]=s[i]+dt/6*(rk_step_matrix[0][i]+2*rk_step_matrix[1][i]+2*rk_step_matrix[2][i]+rk_step_matrix[3][i])
+    for i in funtions.keys(): fin_dict[i]=fin_dict[i]+hd*(rk_step_matrix[0][i]+2*rk_step_matrix[1][i]+2*rk_step_matrix[2][i]+rk_step_matrix[3][i])
     return 0
 
-# ustawienia
-n = 4 # ilość zmiennych
-hours = 64
-tmax = 3600*hours# czas symulacji
-dt = 1
-N = tmax/dt; # ilość kroktów czasowych
-
+# MAIN LOOP
 t = 0;
+dt = data['options']['dt']
 time = []
 wynik = []
+dt = dt/6
+tmax = 3600*data['options']['hours']
+N = (3600*data['options']['hours'])/dt
+rk_step_matrix = [dict() for i in range(5)]
+loop = list(zip(list(range(4)), [0, dt/2, dt/2, dt]))
+pct = 0
 for i in range(int(N)):
-    rk4_vec(t, dt, n, s, pochodne) # to moj step
-    wynik.append(copy.deepcopy(s))
-    t += dt
-    time.append(t)
+    if  (pct := math.floor(100*i/N)) > pct: print(pct)
+    rk4_vec(dt) # to moj step
+    wynik.append(copy.deepcopy(fin_dict))
+    time.append(t:=t+dt)
+    
 
 # PLOT
-[plt.plot(time, [column[i] for column in wynik], label=labels[i]) for i in range(n)]
+input(wynik[-1])
+[plt.plot(time, [column[key] for column in wynik], label=key) for i, key in enumerate(funtions.keys())]
 plt.xlabel('Czas (s)')
 plt.ylabel('Liczba cząsteczek')
-plt.title(f'Symulacja {hours}h')
-plt.xticks(np.arange(0, tmax, step= (lbs := int(tmax/10))), labels=[str(int(hours*i/10)) for i in range(10)])
+plt.title(f"Symulacja {data['options']['hours']}h")
+plt.xticks(np.arange(0, tmax, step=(lbs := int(tmax/10))), labels=[str(int(data['options']['hours']*i/10)) for i in range(10)])
 plt.legend()
+plt.grid(True)
 plt.savefig('wykres.jpg')
